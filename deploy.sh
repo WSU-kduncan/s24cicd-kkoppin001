296c8728e5aa2f38186ed0d504ebe4f7d4babd08
Ubuntu <kkoppin001@gmail.com>
2024-04-18 14:55:28 +0000

    adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define variables
+IMAGE_NAME="kkoppin001/docker-repo-koppin:v.1"
+CONTAINER_NAME="pedantic_volhard"
+
+# Pull the latest image from DockerHub
+docker pull $IMAGE_NAME
+
+# Stop and remove the existing container
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+
+# Run a new container with the latest image
+docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+
