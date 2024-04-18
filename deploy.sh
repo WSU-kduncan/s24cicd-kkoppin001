21024b2da4d1bbbd7d57aeffd610d7964f4865d7
Ubuntu <kkoppin001@gmail.com>
2024-04-18 15:44:43 +0000

    update script

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
 # Run a new container with the latest image
-docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d -p 80:5000--name $CONTAINER_NAME $IMAGE_NAME
 
